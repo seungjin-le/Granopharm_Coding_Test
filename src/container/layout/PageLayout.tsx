e56d5fdfeb720f57417249c84cf112f3b95596bb
Seungjin Lee <dltmdwls154@gmail.com>
2023-05-11 13:47:40 +0900

    feat: @web3-react/core @web3-react/injected-connector ethers 라이브러리 설치

--- src/container/layout/PageLayout.tsx
+++ src/container/layout/PageLayout.tsx
@@ -1,5 +1,15 @@
+import PageHeader from 'container/layout/PageHeader'
+import PageContent from 'container/layout/PageContent'
+import PageFooter from 'container/layout/PageFooter'
+
 const PageLayout = () => {
-  return <div></div>
+  return (
+    <>
+      <PageHeader />
+      <PageContent />
+      <PageFooter />
+    </>
+  )
 }
 
 export default PageLayout

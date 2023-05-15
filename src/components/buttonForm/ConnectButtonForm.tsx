5471251d7b0cbf2c7846949f7214e74e26317f23
LeeSeungJin <dltmdwls154@gmail.com>
2023-05-15 19:35:35 +0900

    feat: Axios 설치, 폰트 없어서 발생하는 경고 주석처리

--- src/components/buttonForm/ConnectButtonForm.tsx
+++ src/components/buttonForm/ConnectButtonForm.tsx
@@ -31,7 +31,7 @@ const CustomAntConnectBtn = styled(Button)`
     width: 170px;
     height: 19px;
     flex-grow: 0;
-    font-family: AirbnbCereal_W_Bd-;
+    //font-family: AirbnbCereal_W_Bd-;
     font-size: 15px;
     font-weight: normal;
     font-stretch: normal;

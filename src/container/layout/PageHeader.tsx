0d6e4b54762d9274cfd562fdab365ffe204bc430
LeeSeungJin <dltmdwls154@gmail.com>
2023-05-15 22:13:10 +0900

    refactor: PageLayout, PageHeader 스타일 수정

--- src/container/layout/PageHeader.tsx
+++ src/container/layout/PageHeader.tsx
@@ -2,33 +2,17 @@ import * as React from 'react'
 import HeaderTitleForm from 'components/texts/HeaderTitleForm'
 import PageHeaderLogo from 'components/images/PageHeaderLogo'
 import ProfileImage from 'components/images/ProfileImage'
-import {Row, Layout} from 'antd'
+import {Layout, Row} from 'antd'
 import styled from 'styled-components'
 
-const PageHeader = () => {
-  const title: string = 'Renaissance Lab.'
-  //const [users, setUsers] = useState<null | HTMLElement>(null)
-  //const [profileMenuBtn, setProfileMenuBtn] = useState(false)
-
-  // const handleOpenProfileMenu = (event: React.MouseEvent<HTMLElement>) => {
-  //   setUsers(event.currentTarget)
-  // }
-  //
-  // const handleCloseProfileMenu = () => {
-  //   setUsers(null)
-  // }
-
+const PageHeader = ({title}: any) => {
   return (
     <CustomAntHeader>
       <Row>
         <PageHeaderLogo />
         <HeaderTitleForm title={title} />
       </Row>
-      <ProfileImage
-      //openProfileMenu={event => handleOpenProfileMenu(event as React.MouseEvent<HTMLElement>)}
-      //closeProfileMenu={handleCloseProfileMenu}
-      //users={users}
-      />
+      <ProfileImage />
     </CustomAntHeader>
   )
 }
@@ -36,11 +20,9 @@ const PageHeader = () => {
 export default PageHeader
 
 const CustomAntHeader = styled(Layout)`
-  display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
-  max-width: 1440px;
   height: 76px;
   padding: 18px 28px 2px 36px;
   background-color: #fff;

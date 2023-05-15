8eb1faad381a5cec2b1843278dca2c6d32c38bc4
LeeSeungJin <dltmdwls154@gmail.com>
2023-05-15 22:20:20 +0900

    feat: PageContent 스타일 추가 ContentCardList를 Props로 받아 출력되도록 기능 추가

--- src/container/layout/PageContent.tsx
+++ src/container/layout/PageContent.tsx
@@ -1,5 +1,19 @@
-const PageContent = () => {
-  return <div></div>
+import React from 'react'
+import styled from 'styled-components'
+import {Layout} from 'antd'
+import ContentCardList from 'components/list/contents/contentCardList'
+
+const PageContent = ({assets}: any) => {
+  return (
+    <CustomAntContent>
+      <ContentCardList assets={assets} />
+    </CustomAntContent>
+  )
 }
 
 export default PageContent
+
+const CustomAntContent = styled(Layout)`
+  padding: 18px 28px 2px 36px;
+  background: #ffffff;
+`
--- src/container/layout/PageFooter.tsx
+++ src/container/layout/PageFooter.tsx
@@ -1,5 +1,59 @@
-const PageFooter = () => {
-  return <div></div>
+import {Layout, Space, Spin} from 'antd'
+import styled from 'styled-components'
+import {memo, useEffect, useState} from 'react'
+import {InfiniteScroll} from 'lodash'
+
+const PageFooter = ({infiniteScroll}: InfiniteScroll) => {
+  const [isBottom, setIsBottom] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+  const handleScroll = () => {
+    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop
+    const scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight
+    const clientHeight = document.documentElement.clientHeight || window.innerHeight
+
+    if (scrollTop + clientHeight >= scrollHeight) {
+      setIsBottom(true)
+    } else {
+      setIsBottom(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (isBottom) {
+      setIsLoading(true)
+      // 스크롤이 맨 아래로 도달했을 때 추가 데이터 요청
+      setTimeout(() => {
+        console.log('Reached bottom of page!')
+        infiniteScroll()
+        setIsLoading(false)
+      }, 2000)
+    }
+  }, [isBottom])
+  return (
+    <CustomAntFooter>
+      {isLoading && (
+        <Space size='middle'>
+          <Spin size='large' />
+        </Space>
+      )}
+    </CustomAntFooter>
+  )
 }
 
-export default PageFooter
+export default memo(PageFooter)
+
+const CustomAntFooter = styled(Layout)`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  height: 76px;
+  padding: 18px 28px 2px 36px;
+  background-color: #fff;
+`

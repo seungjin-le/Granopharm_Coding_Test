199906239c910c8ec9c97744fd3c312d1934fb54
LeeSeungJin <dltmdwls154@gmail.com>
2023-05-15 22:19:04 +0900

    feat: API 요청 모듈 추가

--- src/dataManager/apiMapper.js
+++ src/dataManager/apiMapper.js
@@ -1,11 +1,11 @@
+const API = process.env.REACT_APP_OPENSEA_API
 export const EndPoint = {
-  // ------ 오케이포스 홈페이지
-  GET_V1_TEST_AUTO_LOGIN: `${''}/test/v1/jwt/check/:isManual`,
+  GET_OPENSEA_IMAGES: `${API}/bundles?limit=20&offset=0`,
 }
 
 const ApiMapper = {
   get: {
-    [EndPoint.GET_V1_TEST_AUTO_LOGIN]: {},
+    [EndPoint.GET_OPENSEA_IMAGES]: {},
   },
   post: {},
   patch: {},

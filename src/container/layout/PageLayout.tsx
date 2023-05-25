9d7ba713f8d3643cca63dc165a43899aea3c73e6
LeeSeungJin <dltmdwls154@gmail.com>
2023-05-25 19:17:03 +0900

    feat: PageLayout 에서 useEffect > React-Query로 변경

--- src/container/layout/PageLayout.tsx
+++ src/container/layout/PageLayout.tsx
@@ -3,15 +3,15 @@ import PageFooter from 'container/layout/PageFooter'
 import PageTabs from 'container/layout/PageTabs'
 import styled from 'styled-components'
 import {Layout, Space} from 'antd'
-import React, {memo, useEffect} from 'react'
+import React, {memo} from 'react'
 import {EndPoint} from 'dataManager/apiMapper'
 import {AssetAPI, PageLayoutProps, PageTab} from 'lodash'
 import {useNavigate} from 'react-router-dom'
 import {getData} from 'utils/utility'
-import {useDispatch, useSelector} from 'react-redux'
+import {useDispatch} from 'react-redux'
 import {addCards, resetCards} from 'store/redux/cards/AssetsSlice'
 import {incrementCurrentPage, resetCurrentPage} from 'store/redux/pages/CurrentPage'
-
+import {useQuery} from 'react-query'
 /**
  * 페이지 레이아웃 컴포넌트
  * @param {ReactNode} children - 자식 컴포넌트
@@ -26,12 +26,10 @@ const PageLayout = ({children}: PageLayoutProps) => {
   ]
   const navigate = useNavigate()
   const dispatch = useDispatch()
-  let currentPage = useSelector((state: any) => state.currentPage.value)
+  //let currentPage = useSelector((state: any) => state.currentPage.value)
 
   // 다른 페이지 추가시 API 로직 분리
-  const getDate = async (tabKey?: string) => {
-    console.log(`${tabKey} Page${currentPage}`)
-
+  const getPageData = async () => {
     await getData(EndPoint.GET_OPENSEA_IMAGES)
       .then(({bundles}) => {
         dispatch(
@@ -49,6 +47,12 @@ const PageLayout = ({children}: PageLayoutProps) => {
       })
       .catch(err => console.log(err))
   }
+
+  // 페이지 마운트시 데이터 로드
+  useQuery('getPageData', getPageData, {
+    refetchOnMount: false, // 데이터를 처음에만 불러옴
+  })
+
   // 텝 이동시 tabKey를 이용한 API요청 함수
   const handleOnChangeTap = (tabKey: string) => {
     // URL 이동
@@ -63,18 +67,14 @@ const PageLayout = ({children}: PageLayoutProps) => {
     dispatch(resetCurrentPage())
 
     // TabKey를 사용하여 API 요청
-    getDate(tabKey)
+    getPageData()
   }
 
   const handleOnChangeScroll = () => {
     // 스크롤 최하단에 도달시 추가 데이터 요청
     dispatch(incrementCurrentPage())
-    return getDate()
+    return getPageData()
   }
-
-  useEffect(() => {
-    getDate() // 컴포넌트 마운트시 데이터 로드
-  }, [])
 
   return (
     <CustomAntPageLayout direction='vertical' size={[0, 48]}>
--- src/index.tsx
+++ src/index.tsx
@@ -7,17 +7,20 @@ import {Web3ReactProvider} from '@web3-react/core'
 import GlobalStyleStyled from 'styles/GlobalStyleStyled'
 import {Provider} from 'react-redux'
 import {store} from 'store/store'
+import {QueryClient, QueryClientProvider} from 'react-query'
 
 const getLibrary = (provider: ExternalProvider | JsonRpcFetchFunc): Web3Provider => {
   return new Web3Provider(provider, 'any')
 }
-
+const queryClient = new QueryClient()
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <Web3ReactProvider getLibrary={getLibrary}>
-    <GlobalStyleStyled />
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </Web3ReactProvider>,
+  <QueryClientProvider client={queryClient}>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <GlobalStyleStyled />
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Web3ReactProvider>
+  </QueryClientProvider>,
 )
